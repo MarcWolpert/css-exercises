d033ae7563b7be2849c00873d6ef99cd5c819ed3
Marc Wolpert <mewolpert@gmail.com>
2024-12-24 21:27:06 -0800

    style: Modal animation finished

--- advanced-html-css/animation/02-pop-up/script.js
+++ advanced-html-css/animation/02-pop-up/script.js
@@ -2,10 +2,24 @@ const openButton = document.getElementById('trigger-modal');
 const closeButton = document.getElementById('close-modal');
 
 function toggleModal() {
-  const modalDiv = document.querySelector('.popup-modal');
-  const backdrop = document.querySelector('.backdrop');
-  modalDiv.classList.toggle('show');
-  backdrop.classList.toggle('show');
+	const modalDiv = document.querySelector('.popup-modal');
+	const backdrop = document.querySelector('.backdrop');
+	let isShowing = modalDiv.classList.contains('show') === false;
+	if (isShowing) {
+		modalDiv.style.animationName = 'fade-in';
+		modalDiv.style.animationDuration = '2s';
+		modalDiv.style.animationDirection = 'forwards';
+		modalDiv.style.animationIterationCount = '1';
+		modalDiv.classList.add('show');
+		backdrop.classList.add('show');
+	} else {
+		modalDiv.style.animationName = 'fade-out';
+		modalDiv.style.animationDuration = '2s';
+		modalDiv.style.animationDirection = 'forwards';
+		modalDiv.style.animationIterationCount = '1';
+		modalDiv.classList.remove('show');
+		backdrop.classList.remove('show');
+	}
 }
 
 openButton.addEventListener('click', toggleModal);
